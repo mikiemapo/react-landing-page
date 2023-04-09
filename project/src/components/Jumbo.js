879cc6b9f80b7fbb7a8b1f7febff4f02bf289883
mikiemapo <findmikemm@gmail.com>
2023-04-09 01:31:54 +0000

    fixed copyrighht fixed margin navbar text color ternary code with use state  for jumbo button

--- project/src/components/Jumbo.js
+++ project/src/components/Jumbo.js
@@ -8,20 +8,18 @@ function Jumbo() {
                 <h1 className="display-4">Hello, world!</h1>
                 {
                     !learn ?
-                    <>
-                        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                        <hr className="my-4" />
-                        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                        <p className="lead">
-                            <a onClick={()=>setLearn(!learn)} className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-                        </p> </> : <>
-                        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                        <hr className="my-4" />
-                        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                        <p className="lead">
-                            <a onClick={()=>setLearn(!learn)} className="btn btn-primary btn-lg" href="#" role="button">Learning</a>
-                        </p>    
-                    </>
+                        <>
+                            <p className="lead">This is a simple hero unit, if you click the button all the text will go away .</p>
+                            <hr className="my-4" />
+                            <p>I used terinary and use state for the first time .</p>
+                            <p className="lead">
+                                <a onClick={() => setLearn(!learn)} className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                            </p>
+                        </> :
+
+                        <>
+                            <a onClick={() => setLearn(!learn)} className="btn btn-primary btn-lg" href="#" role="button">Learning</a>
+                        </>
                 }
 
             </div>
